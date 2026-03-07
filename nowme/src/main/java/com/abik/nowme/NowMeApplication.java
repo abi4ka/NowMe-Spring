6b4ba499042308f159deb18d731cc99060128aba
abik <nikita161787@gmail.com>
2026-03-07 14:11:24 +0100

    init project

---
+++ nowme/src/main/java/com/abik/nowme/NowMeApplication.java
@@ -0,0 +1,13 @@
+package com.abik.nowme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowMeApplication.class, args);
+	}
+
+}
---
+++ nowme/src/test/java/com/abik/nowme/NowMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abik.nowme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
