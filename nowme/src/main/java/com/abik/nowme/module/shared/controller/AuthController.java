54ebe8dbce84875bc7a0e0723d9e28e6df616273
abik <nikita161787@gmail.com>
2026-03-10 19:51:04 +0100

    User & Auth system

---
+++ nowme/src/main/java/com/abik/nowme/module/shared/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.abik.nowme.module.shared.controller;
+
+import com.abik.nowme.module.shared.service.AuthService;
+import com.abik.nowme.module.user.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public void register(@RequestBody UserDto.RegisterRequest request) {
+        authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public UserDto.AuthResponse login(@RequestBody UserDto.LoginRequest request) {
+        return authService.login(request);
+    }
+}
