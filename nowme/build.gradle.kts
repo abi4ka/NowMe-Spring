7c55b47f96d249d91f3c47093c64357f815565f4
abik <nikita161787@gmail.com>
2026-03-10 19:32:00 +0100

    Update build.gradle.kts

--- nowme/build.gradle.kts
+++ nowme/build.gradle.kts
@@ -22,6 +22,27 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// Validation (Jakarta)
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	// Spring
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// Lombok
+	compileOnly("org.projectlombok:lombok:1.18.42")
+	annotationProcessor("org.projectlombok:lombok:1.18.42")
+
+	// JPA + Hibernate
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	// PostgreSQL
+	runtimeOnly("org.postgresql:postgresql")
+
+	// JWT
+	implementation("com.auth0:java-jwt:4.4.0")
+
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 }
 
 tasks.withType<Test> {
