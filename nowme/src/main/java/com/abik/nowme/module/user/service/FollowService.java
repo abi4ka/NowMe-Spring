32b8e5dc1b12ac0a5163a95ac8151287f6a42d97
Gabriela <gvazquezi24@fpcoruna.afundacion.org>
2026-04-02 12:02:42 +0200

    Create FollowService.java

---
+++ nowme/src/main/java/com/abik/nowme/module/user/service/FollowService.java
@@ -0,0 +1,49 @@
+package com.abik.nowme.module.user.service;
+
+import com.abik.nowme.module.shared.service.JwtService;
+import com.abik.nowme.module.user.entity.User;
+import com.abik.nowme.module.user.entity.UserFollow;
+import com.abik.nowme.module.user.repository.UserFollowRepository;
+import com.abik.nowme.module.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FollowService {
+
+    private final UserRepository userRepository;
+    private final UserFollowRepository followRepository;
+    private final JwtService jwtService;
+
+    public void follow(String token, Long userIdToFollow) {
+
+        String username = jwtService.extractUsername(token);
+
+        User follower = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        User user = userRepository.findById(userIdToFollow)
+                .orElseThrow(() -> new RuntimeException("User to follow not found"));
+
+        if (follower.getId().equals(user.getId())) {
+            throw new RuntimeException("You can not follow yourself");
+        }
+
+        if (followRepository.existsByUser_IdAndFollower_Id(user.getId(), follower.getId())) {
+            throw new RuntimeException("You are already following");
+        }
+
+        UserFollow follow = new UserFollow();
+        follow.setUser(user);
+        follow.setFollower(follower);
+
+        followRepository.save(follow);
+
+        user.setFollowers(user.getFollowers() + 1);
+        follower.setFollowing(follower.getFollowing() + 1);
+
+        userRepository.save(user);
+        userRepository.save(follower);
+    }
+}
