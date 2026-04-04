ac500f17b518639c2017e8f21393bcdf2efe6b61
Gabriela <gvazquezi24@fpcoruna.afundacion.org>
2026-04-04 14:52:18 +0200

    Rename UserFollow to Follow

--- nowme/src/main/java/com/abik/nowme/module/user/repository/UserFollowRepository.java
+++ nowme/src/main/java/com/abik/nowme/module/user/repository/UserFollowRepository.java
@@ -1,9 +1,9 @@
 package com.abik.nowme.module.user.repository;
 
-import com.abik.nowme.module.user.entity.UserFollow;
+import com.abik.nowme.module.user.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserFollowRepository extends JpaRepository<UserFollow, Long> {
+public interface UserFollowRepository extends JpaRepository<Follow, Long> {
 
     boolean existsByUser_IdAndFollower_Id(Long userId, Long followerId);
 
