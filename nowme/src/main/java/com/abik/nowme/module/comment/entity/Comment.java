875b9d3f650378c1b9e84820b2fbbd7f0eb17366
Gabriela Vázquez <gvazquezi24@fpcoruna.afundacion.org>
2026-04-08 13:08:36 +0200

    Create Comment.java

---
+++ nowme/src/main/java/com/abik/nowme/module/comment/entity/Comment.java
@@ -0,0 +1,34 @@
+package com.abik.nowme.module.comment.entity;
+
+import com.abik.nowme.module.nowme.entity.Nowme;
+import com.abik.nowme.module.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Nowme nowme;
+
+    private String content;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
